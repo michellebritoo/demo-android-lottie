d4740aa17ead7db5a11fbf31d90c1ea35b28cf50
Michelle Brito Cunha MCN <michelle.brito@iteris.com.br>
2021-11-17 12:08:46 -0300

    Add a Transform button

--- app/src/main/java/br/com/demo/demolottie/MainActivity.kt
+++ app/src/main/java/br/com/demo/demolottie/MainActivity.kt
@@ -2,11 +2,24 @@ package br.com.demo.demolottie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.BtnTransform
+import kotlinx.android.synthetic.main.activity_main.animationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        BtnTransform.setOnClickListener {
+            if(!animationView.isAnimating)
+                animationView.playAnimation()
+            else
+                animationView.pauseAnimation()
+        }
     }
 }
