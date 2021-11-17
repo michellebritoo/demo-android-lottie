175c7b0ca29c7d0ae5c1693b493cc9271ba996de
Michelle Brito Cunha MCN <michelle.brito@iteris.com.br>
2021-11-17 12:25:50 -0300

    Add a change animation button

--- app/src/main/java/br/com/demo/demolottie/MainActivity.kt
+++ app/src/main/java/br/com/demo/demolottie/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.BtnTransform
 import kotlinx.android.synthetic.main.activity_main.animationView
+import kotlinx.android.synthetic.main.activity_main.BtnChange
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,10 +17,17 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
 
         BtnTransform.setOnClickListener {
-            if(!animationView.isAnimating)
+            if (!animationView.isAnimating)
                 animationView.playAnimation()
             else
                 animationView.pauseAnimation()
+        }
+
+        BtnChange.setOnClickListener {
+            animationView.setAnimationFromUrl(
+                "https://assets1.lottiefiles.com/packages/lf20_sx5e0zms.json"
+            )
+            animationView.playAnimation()
         }
     }
 }
